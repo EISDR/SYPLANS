b24ca39d3807d1821e1bc836b500c8008449b0b0
Angel Luis Rodriguez Polanco <angel.rodriguez810@gmail.com>
2025-08-07 16:30:58 -0400

    .
    .

--- 7-plugins/application/controllers/CO_BASE.js
+++ 7-plugins/application/controllers/CO_BASE.js
@@ -510,12 +510,13 @@ app.controller('baseController', function ($scope, $http, $compile, $controller)
                     {field: "compania_id", value: intersession.compania_id},
                 ]
             });
-            HINTSDB = await BASEAPI.firstp("hint_fields", {
+            HINTSDB = await BASEAPI.listp("hint_fields", {
                 orderby: "company",
                 order: "desc",
                 where: [
                     {field: "company", value: intersession.compania_id},
-                ]
+                ],
+                limit: 0
             });
             var poa_selected = await BASEAPI.firstp("vw_poa", {
                 where: [
